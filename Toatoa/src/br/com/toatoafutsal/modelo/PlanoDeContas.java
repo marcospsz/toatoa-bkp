369b0f4b60280406f277f8e60339ff86d9437f33
marcospsz <marcospsz@gmail.com>
2017-03-26 23:02:25 -0300

    versao formatando diretorioc

---
+++ Toatoa/src/br/com/toatoafutsal/modelo/PlanoDeContas.java
@@ -0,0 +1,24 @@
+package br.com.toatoafutsal.modelo;
+
+public class PlanoDeContas {
+	
+	private int numeroConta;
+	private String descricaoConta;
+	
+	public int getNumeroConta() {
+		return numeroConta;
+	}
+	public void setNumeroConta(int numeroConta) {
+		this.numeroConta = numeroConta;
+	}
+	
+	public String getDescricaoConta() {
+		return descricaoConta;
+	}
+	public void setDescricaoConta(String descricaoConta) {
+		this.descricaoConta = descricaoConta;
+	}
+	
+	
+
+}
